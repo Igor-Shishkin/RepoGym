fd6d8473947383c88d37b59e800e462ae4ed3324
Igor <130922573+Igor-Shishkin@users.noreply.github.com>
2023-07-04 17:50:03 +0200

    Training with CodeWars

---
+++ src/main/java/selfStudyJava/codeWars/kyu6/BinaryRepresentation.java
@@ -0,0 +1,18 @@
+package selfStudyJava.codeWars.kyu6;
+
+/*
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+ */
+public class BinaryRepresentation {
+    public static void main(String[] args) {
+        int number = 1234;
+        String binary = Integer.toBinaryString(number);
+        int counter = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            counter = (binary.charAt(i) == '1') ? counter += 1 : counter;
+        }
+        System.out.println(counter);
+    }
+}
