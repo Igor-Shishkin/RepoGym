e5d592db509a5c9ec9ce45eb994d079a1cb1d3a0
Igor <130922573+Igor-Shishkin@users.noreply.github.com>
2023-07-10 17:22:24 +0200

    addition to the previous task
    One more solution plus description

--- src/main/java/selfStudyJava/codeWars/kyu5/MaximumSubarraySum.java
+++ src/main/java/selfStudyJava/codeWars/kyu5/MaximumSubarraySum.java
@@ -3,6 +3,18 @@ package selfStudyJava.codeWars.kyu5;
 public class MaximumSubarraySum {
     public static void main(String[] args) {
         int[] arr = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        /*
+        The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array
+        or list of integers:
+
+Max.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+// should be 6: {4, -1, 2, 1}
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array.
+If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+         */
+
         if (arr.length==0) {
             System.out.println("0");;
         }
@@ -17,7 +29,19 @@ public class MaximumSubarraySum {
             }
         }
         System.out.println(sumMax);
+
+
+        /*
+
+                int m = 0, a = 0, s = 0;
+        for(int e : arr) {
+            s += e;
+            m = Math.min(s, m);
+            a = Math.max(a, s - m);
+        }
+        System.out.println(a);;
+         */
 //        return sumMax;
     }
-    
+
 }
