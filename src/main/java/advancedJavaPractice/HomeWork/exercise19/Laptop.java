4f0668e4baeec534fd0bdbb3d77a6d5f9b83b125
Igor <130922573+Igor-Shishkin@users.noreply.github.com>
2023-07-11 06:43:39 +0200

    Training with EQUALS and HASHCODE
    homeWork from SDA - Java Advanced, Practice

---
+++ src/main/java/advancedJavaPractice/HomeWork/exercise19/Laptop.java
@@ -0,0 +1,33 @@
+package advancedJavaPractice.HomeWork.exercise19;
+
+import java.util.Objects;
+
+public class Laptop extends Computer{
+    private final int batteryWh;
+
+    public Laptop(String model, String firma, Processor processor, int ramGB, VideoCard videoCard, int battery) {
+        super(model, firma, processor, ramGB, videoCard);
+        this.batteryWh = battery;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Laptop)) return false;
+        if (!super.equals(o)) return false;
+        Laptop laptop = (Laptop) o;
+        return batteryWh == laptop.batteryWh;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), batteryWh);
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop {" + super.toString() +
+                "battery=" + batteryWh +
+                '}';
+    }
+}
---
+++ src/main/java/advancedJavaPractice/HomeWork/exercise19/Main.java
@@ -0,0 +1,55 @@
+package advancedJavaPractice.HomeWork.exercise19;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer computer1 = new Computer("Aspire 3", "Acer",
+                new Processor("Intel", 2400,4), 8,
+                new VideoCard("NVidea", 2048));
+        Computer computer2 = new Computer("Aspire 3", "Acer",
+                new Processor("Intel", 2400,4), 8,
+                new VideoCard("NVidea", 2048));
+        Computer computer3 = new Computer("Aspire 3", "Acer",
+                new Processor("Intel", 2400,4), 12,
+                new VideoCard("NVidea", 2048));
+
+        Laptop laptop1 = new Laptop("ASUS X515JA", "Asus",
+                new Processor("Intel Core i5", 3000,4), 8,
+                new VideoCard("NVIDIA GeForce GTX 1650", 4096), 48);
+        Laptop laptop2 = new Laptop("Aspire 3", "Acer",
+                new Processor("Intel", 2400,4), 12,
+                new VideoCard("NVidea", 2048), 48);
+        Laptop laptop3 = new Laptop("ASUS X515JA", "Asus",
+                new Processor("Intel Core i5", 3000,4), 8,
+                new VideoCard("NVIDIA GeForce GTX 1650", 4096), 48);
+
+        System.out.println(computer1);
+        System.out.println(computer2);
+
+        System.out.println("if 'computer1'=='computer2':   " + computer1.equals(computer2));
+
+        System.out.println("************************************");
+        System.out.println(computer2);
+        System.out.println(computer3);
+        System.out.println("if 'computer2'=='computer3':   " + computer3.equals(computer2));
+
+        System.out.println("************************************");
+        System.out.println(laptop1);
+        System.out.println(laptop2);
+        System.out.println("if 'laptop1'=='laptop2':   " + laptop1.equals(laptop2));
+
+        System.out.println("************************************");
+        System.out.println(laptop1);
+        System.out.println(laptop3);
+        System.out.println("if 'laptop1'=='laptop3':   " + laptop1.equals(laptop3));
+
+        System.out.println("************************************");
+        System.out.println(computer3);
+        System.out.println(laptop2);
+        System.out.println("if 'computer3'=='laptop2':   " + computer3.equals(laptop2));
+
+        System.out.println("************************************");
+        System.out.println(laptop2);
+        System.out.println(computer3);
+        System.out.println("if 'laptop2'=='computer3':   " + laptop2.equals(computer3));
+    }
+}
