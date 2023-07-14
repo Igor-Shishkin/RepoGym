b4bea4753cde2547b997ca99f7e35067633f2c33
Igor <130922573+Igor-Shishkin@users.noreply.github.com>
2023-07-14 16:40:00 +0200

    The solution of 'Breadcrumb Generator' problem
    The solution of 'Breadcrumb Generator' problem from CodeWars - 4 kyu!

--- src/main/java/selfStudyJava/codeWars/kyu4/breadcrumbGenerator/BreadcrumbGenerator.java
+++ src/main/java/selfStudyJava/codeWars/kyu4/breadcrumbGenerator/BreadcrumbGenerator.java
@@ -5,46 +5,111 @@ import java.util.regex.Pattern;
 
 public class BreadcrumbGenerator {
     public static void main(String[] args) {
-        String url = "www.microsoft.com/docs/index.htm";
-        String separator = " : ";
-        if (url.lastIndexOf("/index.") == url.lastIndexOf("/")) {
+        String url = "www.microsoft";
+        String separator = " * ";
+        System.out.println("<<[span class=\"active\">HOME]</span>>");
+        if (url.lastIndexOf("/")==url.length()-1) {
             url = url.substring(0,url.lastIndexOf("/"));
         }
-        String[] PartsUrl = url.split("/");
+        if (url.contains("index.")) {
+            url = url.substring(0,url.lastIndexOf("/"));
+        }
+        if (url.contains("//")) {
+            url = url.substring(url.indexOf("/")+2);
+        }
+//        int length;
+        System.out.println(url);
+//        String[] devided = url.split("/");
+//        if (devided[devided.length - 1] == "") {
+//            length = devided.length-1;
+//        } else {
+//            length = devided.length-2;
+//        }
+        String[] partsUrl = url.split("/");
 
-        String result = null;
-        if (PartsUrl.length==2) {
-            String active =
-            result = String.format("<a href=\"/\">HOME</a>%s<span class=\"active\">%s</span>",
-                 separator, PartsUrl[1].toUpperCase());
+        for (int i = 0; i < partsUrl.length; i++) {
+            if (partsUrl[i].contains("?")){
+                partsUrl[i] = partsUrl[i].substring(0,partsUrl[i].indexOf("?"));
+            }
+            if (partsUrl[i].contains("#")){
+                partsUrl[i] = partsUrl[i].substring(0,partsUrl[i].indexOf("#"));
+            }
+            if (partsUrl[i].contains(".")){
+                partsUrl[i] = partsUrl[i].substring(0,partsUrl[i].indexOf("."));
+            }
         }
 
-        String t1 = "123";
+        int length = (partsUrl[partsUrl.length-1].equalsIgnoreCase("")) ? partsUrl.length-1 : partsUrl.length;
 
-        System.out.println(Arrays.toString(PartsUrl));
+        StringBuilder result = new StringBuilder();
+        result.append("<a href=\"/\">HOME</a>");
+
+        for (int i = 1; i < length-1; i++) {
+            result.append(separator);
+            result.append("<a href=\"/");
+            result.append(partsUrl[i]);
+            result.append("/\">");
+            result.append(correctURL(partsUrl[i]));
+            result.append("</a>");
+        }
+
+        result.append(separator);
+        result.append("<span class=\"active\">");
+        result.append(correctURL(partsUrl[length-1]));
+        result.append("</span>");
+
+
+
+//
+//        String result = null;
+//        if (partsUrl.length==2) {
+//            result = String.format("<a href=\"/\">HOME</a>%s<span class=\"active\">%s</span>",
+//                 separator, correctURL(partsUrl[1]));
+//        } else {
+//            if (partsUrl[2].contains(".")) {
+//                partsUrl[2] = partsUrl[2].substring(0,partsUrl[2].indexOf("."));
+//            }
+//            result = String.format("<a href=\"/\">HOME</a>%s<a href=\"/%s/\">%s</a>%s<span class=\"active\">%s</span>",
+//                    separator, partsUrl[1], correctURL(partsUrl[1]), separator, correctURL(partsUrl[2]));
+//        }
+
+        System.out.println(Arrays.toString(partsUrl));
         System.out.println(result);
+        System.out.println("<a href=\"/\">HOME</a> * <span class=\"active\">DOCS</span>");
+
+        System.out.println(result.toString().equals("<a href=\"/\">HOME</a> * <span class=\"active\">DOCS</span>"));
 
 
     }
-
-//    public static String generate_bc(String url, String separator) {
-//        String[] partsUrl = url.split("/");
-//
-//
-//        return String.format("<a href=\"/\">HOME</a> %s <a href=\"/%s",
-//                separator, );
-//    }
-
-
-    public static String moreThan30symbols (String text) {
+    public static String correctURL (String text) {
         if (text.length() <= 30) {
+            text = text.toUpperCase().replaceAll("-"," ");
             return text;
         } else {
             String[] words = text.split("-");
-            String ignoreWords = "[()]"
-            Pattern pattern = Pattern.compile()
-            for ()
-            return ;
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].equalsIgnoreCase("the") ||
+                        words[i].equalsIgnoreCase("of") ||
+                        words[i].equalsIgnoreCase("in") ||
+                        words[i].equalsIgnoreCase("from") ||
+                        words[i].equalsIgnoreCase("by") ||
+                        words[i].equalsIgnoreCase("with") ||
+                        words[i].equalsIgnoreCase("and") ||
+                        words[i].equalsIgnoreCase("or") ||
+                        words[i].equalsIgnoreCase("for") ||
+                        words[i].equalsIgnoreCase("to") ||
+                        words[i].equalsIgnoreCase("at") ||
+                        words[i].equalsIgnoreCase("a")) {
+                    words[i] = "";
+                } else {
+                    words[i] = String.valueOf(words[i].toUpperCase().charAt(0));
+                }
+            }
+            StringBuilder result = new StringBuilder();
+            for (String word : words) {
+                result.append(word);
+            }
+            return result.toString();
         }
 
     }
