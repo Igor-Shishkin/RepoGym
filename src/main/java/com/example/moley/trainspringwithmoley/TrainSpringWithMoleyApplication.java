c2aee5030af130e18753e714245c1a2978708640
Igor <igor.fizyk@gmail.com>
2023-10-09 10:54:26 +0200

    train Spring Framework with Frank Moley

--- src/main/java/com/example/moley/trainspringwithmoley/TrainSpringWithMoleyApplication.java
+++ src/main/java/com/example/moley/trainspringwithmoley/TrainSpringWithMoleyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TrainSpringWithMoleyApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TrainSpringWithMoleyApplication.class, args);
 	}
 
