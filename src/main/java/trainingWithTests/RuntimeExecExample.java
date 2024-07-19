5f73038ee07dc74212be5e0fd90dd643bd948f1e
Igor <igor.fizyk@gmail.com>
2024-07-19 17:02:44 +0200

    training

---
+++ src/main/java/trainingWithTests/RuntimeExecExample.java
@@ -0,0 +1,30 @@
+package trainingWithTests;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RuntimeExecExample {
+    public static void main(String[] args) {
+        try {
+            // Uruchomienie komendy systemowej (np. "ls" na Linux/Unix lub "dir" na Windows)
+            Process process = Runtime.getRuntime().exec("dir -la");
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with code : " + exitCode);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
